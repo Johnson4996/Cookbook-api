e429bcc4c29b859cb77c1d8b1a08f090bd88a942
Johnson4996 <Johnson.4996@gmail.com>
2020-12-11 14:54:13 -0600

    now have auth working and recipe viewset

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+from cookbookapi.views.recipe import Recipes
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.urls.conf import include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from cookbookapi.views import *
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'recipes', Recipes, 'recipes')
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'^', include(router.urls)),
+    url(r'^register$', register_user),
+    url(r'^login$', login_user),
+    url(r'^api-token-auth$', obtain_auth_token),
+    url(r'^api-auth', include('rest_framework.urls', namespace='rest_framework')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
